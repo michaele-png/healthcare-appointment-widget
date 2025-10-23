4879a484b7d84782693dd7cf93f3ce174a7aca63
michaele-png <michael.e@caprium.com>
2025-10-23 16:54:34 -0500

    Update nexhealth.js

--- backend/src/lib/nexhealth.js
+++ backend/src/lib/nexhealth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const SUBDOMAIN = process.env.NEXHEALTH_SUBDOMAIN;
 
 export const nh = axios.create({
-  baseURL: process.env.NEXHEALTH_API_BASE,  // sandbox or prod
+  baseURL: process.env.NEXHEALTH_API_BASE,
   headers: {
     Authorization: `Bearer ${process.env.NEXHEALTH_API_KEY}`,
     Accept: 'application/vnd.Nexhealth+json;version=20240412',
