6f3e97d73f34003de571ac82f9562d2f5c5d6804
michaele-png <michael.e@caprium.com>
2025-10-31 00:01:30 -0500

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppointmentWidget } from './AppointmentWidget';
+import AppointmentWidget from './AppointmentWidget';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppointmentWidget practiceId="demo-practice" clinicName="Acme Clinic" brandHue={212} />
-  </React.StrictMode>
+    <AppointmentWidget />
+  </React.StrictMode>,
 );
